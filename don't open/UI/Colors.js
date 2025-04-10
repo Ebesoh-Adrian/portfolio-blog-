24f5b83880f6b5ed44ab6680fa1fad516080b646
Ebesoh-Adrian <adriancoach979@gmail.com>
2025-04-10 08:11:29 +0100

    Coders Hub

---
+++ don't open/UI/Colors.js
@@ -0,0 +1,138 @@
+// First color
+#6DE1D2
+#FFD63A
+#FFA955
+#F75A5A
+// https://colorhunt.co/palette/6de1d2ffd63affa955f75a5a
+
+
+// Second color
+
+#F7CFD8
+#F4F8D3
+#A6D6D6
+#8E7DBE
+// https://colorhunt.co/palette/f7cfd8f4f8d3a6d6d68e7dbe
+
+// Third color
+
+#8F87F1
+#C68EFD
+#E9A5F1
+#FED2E2
+rgb(143, 135, 241)
+rgb(198, 142, 253)
+rgb(233, 165, 241)
+rgb(254, 210, 226)
+// blue purple peach gradient kids light
+
+// https://colorhunt.co/palette/8f87f1c68efde9a5f1fed2e2
+
+// Fourth color
+#B17F59
+#A5B68D
+#C1CFA1
+#EDE8DC
+rgb(177, 127, 89)
+rgb(165, 182, 141)
+rgb(193, 207, 161)
+rgb(237, 232, 220)
+// brown sage grey pastel earth light food winter
+
+// https://colorhunt.co/palette/3f7d58efefefef9651ec5228
+
+//Fifth color
+#3F7D58
+#EFEFEF
+#EF9651
+#EC5228
+rgb(63, 125, 88)
+rgb(239, 239, 239)
+rgb(239, 150, 81)
+rgb(236, 82, 40)
+// green grey orange nature happy kids food retro
+// https://colorhunt.co/palette/3f7d58efefefef9651ec5228
+
+//Sixth color
+
+#FFEDFA
+#E195AB
+#DE3163
+#CCDF92
+rgb(255, 237, 250)
+rgb(225, 149, 171)
+rgb(222, 49, 99)
+rgb(204, 223, 146)
+
+// pink red green pastel light happy kids food
+// https://colorhunt.co/palette/ffedfae195abde3163ccdf92
+
+// Seventh color
+#000000
+#222222
+#1DCD9F
+#169976
+rgb(0, 0, 0)
+rgb(34, 34, 34)
+rgb(29, 205, 159)
+rgb(22, 153, 118)
+
+// black green light dark happy kids food
+// https://colorhunt.co/palette/000000222221dcd9f169976
+
+// Eighth color
+#F1EFEC
+#D4C9BE
+#123458
+#030303
+rgb(241, 239, 236)
+rgb(212, 201, 190)
+rgb(18, 52, 88)
+rgb(3, 3, 3)
+
+// light brown blue dark happy kids food
+// https://colorhunt.co/palette/f1efecd4c9be123458030303
+
+// Ninth color
+
+#27445D
+#497D74
+#71BBB2
+#EFE9D5
+rgb(39, 68, 93)
+rgb(73, 125, 116)
+rgb(113, 187, 178)
+rgb(239, 233, 213)
+
+// blue green light brown happy kids food
+// https://colorhunt.co/palette/27445d497d7471bbefef9d5
+
+// Tenth color
+#1A1A1D
+#3B1C32
+#6A1E55
+#A64D79
+rgb(26, 26, 29)
+rgb(59, 28, 50)
+rgb(106, 30, 85)
+rgb(166, 77, 121)
+
+// black purple pink light happy kids food
+// https://colorhunt.co/palette/1a1a1d3b1c326a1e55a64d79
+
+// How to mix various colors on a Design System
+//example applying the various colors to a design system
+// the colors are used in the following way:
+
+// - Primary color: #6DE1D2
+// - Secondary color: #FFD63A
+// - Accent color: #FFA955
+// - Background color: #F75A5A
+// - Text color: #FFFFFF
+// - Border color: #000000
+// - Shadow color: #000000
+// - Highlight color: #FFD63A
+// - Disabled color: #6DE1D2
+// - Error color: #F75A5A
+
+// the Primary color will be used on like buttons, links, and other interactive elements. The Secondary color will be used for hover states and active states. The Accent color will be used for highlights and important information. The Background color will be used for the main background of the app. The Text color will be used for all text elements. The Border color will be used for borders and dividers. The Shadow color will be used for shadows and depth effects. The Highlight color will be used for selected items and important information. The Disabled color will be used for disabled elements. The Error color will be used for error messages and alerts.
